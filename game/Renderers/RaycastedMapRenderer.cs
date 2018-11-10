afbd1efe572fe230c67d367c47a199b56bd12d0e
Mark van der Wal <wilds.mark@gmail.com>
2018-11-10 17:07:19 +0100

    fixed some bugs

--- game/Renderers/RaycastedMapRenderer.cs
+++ game/Renderers/RaycastedMapRenderer.cs
@@ -1,4 +1,5 @@
 using System;
+using Comora;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using TiledSharp;
@@ -10,12 +11,12 @@ namespace game
         private readonly float FOV;
         private Texture2D blankTexture;
         private Viewport viewport;
+        private int cellSize = 32;
 
         private struct raycastData
         {
-            public float wallFraction;
+            public Vector2 normal;
             public float distance;
-            public Vector2 tileCoordinates;
         }
 
         public RaycastedMapRenderer(Viewport view, Texture2D blank, float fov)
@@ -39,7 +40,6 @@ namespace game
 
             TmxLayer wallLayer = mapData.Layers["walls"];
             float halfFov = FOV / 2;
-            int cellSize = 32;
 
             float dtp = viewport.Width / 2 * (float) Math.Tan(halfFov);
 
@@ -61,7 +61,7 @@ namespace game
             
             // draw fake floor for now
             spriteBatch.Draw(blankTexture, new Rectangle(0, viewport.Height / 2, viewport.Width, viewport.Height), Color.ForestGreen);
-
+            
             // draw all slices
             for (int x = 0; x < viewport.Width; x++)
             {
@@ -77,9 +77,12 @@ namespace game
                 int sliceHeight = (int) (cellSize / distance * dtp);
 
                 Rectangle slice = new Rectangle(x, viewport.Height / 2 - sliceHeight / 2, 1, sliceHeight);
-                spriteBatch.Draw(blankTexture, slice, Color.Orange);
-            }
 
+                float dot = Vector2.Dot(castData.Value.normal, Vector2.UnitX);
+                bool isSide = dot < 0.00001f || dot > -0.00001f;
+                spriteBatch.Draw(blankTexture, slice, isSide ? Color.Gray: Color.White);
+            }
+            
             RenderMinimap(spriteBatch, map, wallLayer, new Vector2(position.X / cellSize, position.Y / cellSize),
                 position, orientation);
         }
@@ -87,8 +90,8 @@ namespace game
         private void RenderMinimap(SpriteBatch spriteBatch, Map map, TmxLayer wallLayer, Vector2 tilecoord, Vector2 pos,
             float angle)
         {
-            int cellSize = 8;
-            int halfCellSize = cellSize / 2;
+            int miniCellSize = 8;
+            int halfCellSize = miniCellSize / 2;
             if (!wallLayer.Visible)
                 return;
 
@@ -98,21 +101,21 @@ namespace game
                 if (tileset == null)
                     continue;
 
-                spriteBatch.Draw(blankTexture, new Rectangle(cellSize + tile.X * cellSize,
-                        cellSize + tile.Y * cellSize, cellSize, cellSize),
+                spriteBatch.Draw(blankTexture, new Rectangle(miniCellSize + tile.X * miniCellSize,
+                        miniCellSize + tile.Y * miniCellSize, miniCellSize, miniCellSize),
                     new Rectangle(0, 0, 1, 1), Color.White);
             }
 
-            Rectangle needleDestination = new Rectangle((int) (cellSize + halfCellSize + pos.X / 32 * cellSize),
-                (int) (cellSize + halfCellSize + pos.Y / 32 * cellSize),
-                cellSize, 2);
+            Rectangle needleDestination = new Rectangle((int) (miniCellSize + halfCellSize + pos.X / cellSize * miniCellSize),
+                (int) (miniCellSize + halfCellSize + pos.Y / cellSize * miniCellSize),
+                miniCellSize, 2);
 
             spriteBatch.Draw(blankTexture, needleDestination, null, Color.Red, angle, new Vector2(-1, 0),
                 SpriteEffects.None, 0);
 
             spriteBatch.Draw(blankTexture,
-                new Rectangle((int) (cellSize + tilecoord.X * cellSize), (int) (cellSize + tilecoord.Y * cellSize),
-                    cellSize, cellSize), Color.Black);
+                new Rectangle((int) (miniCellSize + tilecoord.X * miniCellSize), (int) (miniCellSize + tilecoord.Y * miniCellSize),
+                    miniCellSize, miniCellSize), Color.Black);
         }
 
         private raycastData? GetIntersectionData(Vector2 position, float angle,
@@ -121,7 +124,6 @@ namespace game
             float cos = (float) Math.Cos(angle);
             float sin = (float) Math.Sin(angle);
             float maxViewDistance = 2048;
-            float cellSize = 32;
 
             Vector2 tileCoords = new Vector2((float) Math.Floor(position.X / cellSize),
                 (float) Math.Floor(position.Y / cellSize));
@@ -130,8 +132,8 @@ namespace game
             float signY = Math.Sign(sin);
 
             // get first point delta's as start delta
-            float fX = signX < 0 ? tileCoords.X * cellSize : tileCoords.X * cellSize + cellSize;
-            float fY = signY < 0 ? tileCoords.Y * cellSize : tileCoords.Y * cellSize + cellSize;
+            float fX = signX <= 0 ? tileCoords.X * cellSize : tileCoords.X * cellSize + cellSize;
+            float fY = signY <= 0 ? tileCoords.Y * cellSize : tileCoords.Y * cellSize + cellSize;
 
             float startX = Math.Abs((fX - position.X) / cos);
             float startY = Math.Abs((fY - position.Y) / sin);
@@ -139,33 +141,37 @@ namespace game
             float deltaX = Math.Abs(cellSize / cos);
             float deltaY = Math.Abs(cellSize / sin);
 
-            raycastData castData = new raycastData();
+            raycastData castData = new raycastData {normal = new Vector2()};
+            float t = 0.0f;
             while (true)
-            {
-                float t;
+            {                   
+                if (t >= maxViewDistance)
+                    return null;
+                
+                if (isSolid.Invoke(tileCoords))
+                    break;
+                
                 if (startX <= startY)
                 {
-                    startX += deltaX;
                     t = startX;
+                    startX += deltaX;
                     tileCoords.X += signX;
+                    castData.normal.X = -signX;
+                    castData.normal.Y = 0;
                 }
                 else
                 {
-                    startY += deltaY;
                     t = startY;
+                    startY += deltaY;
                     tileCoords.Y += signY;
+                    castData.normal.X = 0;
+                    castData.normal.Y = -signY;
                 }
-
-                if (t >= maxViewDistance)
-                    return null;
-
-                if (!isSolid.Invoke(tileCoords))
-                    continue;
-
+                
                 castData.distance = t;
-                castData.tileCoordinates = tileCoords;
-                return castData;
             }
+            
+            return castData;
         }
 
         private bool HasProperLayers(TmxMap mapData)
