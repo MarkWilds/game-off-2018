31b9b030a11b4368161789a15f969393fe220c45
WouterHeslinga <Wouter_Heslinga@hotmail.com>
2018-11-26 17:45:32 +0100

    Made screenshaker code in line with the rest of the code base

--- game/Screens/ScreenShaker.cs
+++ game/Screens/ScreenShaker.cs
@@ -5,53 +5,50 @@ namespace game.Screens
 {
     public static class StaticScreenShaker
     {
-        private static ScreenShaker _instance;
+        private static ScreenShaker instance;
 
-        public static ScreenShaker Instance => _instance ?? (_instance = new ScreenShaker());
+        public static ScreenShaker Instance => instance ?? (instance = new ScreenShaker());
     }
     
     
     public class ScreenShaker
     {
-        private float _radius;
-        private float _angle;
-        private int _milliseconds;
+        private float radius;
+        private float angle;
+        private int milliseconds;
 
         public void Shake(int milliseconds, float radius)
         {
-            if (_radius < radius)
-                _radius = radius;
+            if (this.radius < radius)
+                this.radius = radius;
 
-            if (_radius < radius)
-                _radius = radius;
-
-            if (_milliseconds < milliseconds)
-                _milliseconds = milliseconds;
+            if (this.milliseconds < milliseconds)
+                this.milliseconds = milliseconds;
         }
 
         public void Stop()
         {
-            _radius = 0;
-            _angle = 0;
-            _milliseconds = 0;
+            radius = 0;
+            angle = 0;
+            milliseconds = 0;
         }
 
         public void Update(GameTime time)
         {
-            if (_milliseconds <= 0)
+            if (milliseconds <= 0)
                 return;
 
-            _radius -= _radius / 3;
-            _angle += (150 + (new Random().Next(60)));
-            _milliseconds -= time.ElapsedGameTime.Milliseconds;
+            radius -= radius / 3;
+            angle += (150 + (new Random().Next(60)));
+            milliseconds -= time.ElapsedGameTime.Milliseconds;
         }
 
         public Vector2 GetOffset()
         {
-            if (_milliseconds <= 0)
+            if (milliseconds <= 0)
                 return new Vector2();
             
-            var offset = new Vector2((float)(Math.Sin(_angle) * _radius), (float)(Math.Cos(_angle) * _radius));
+            var offset = new Vector2((float)(Math.Sin(angle) * radius), (float)(Math.Cos(angle) * radius));
             return offset;
         }
     }
